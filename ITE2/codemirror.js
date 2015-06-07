da04f282d35aad8c46a4fabcf074a522360f8539
Antosik <grig_anton96@mail.ru>
2015-06-07 19:49:19 +0400

    ITE 2

---
+++ ITE2/codemirror.js
@@ -0,0 +1,167 @@
+$(function () {
+    $("#volumestree").jstree({ "plugins" : ["themes","html_data","ui"], "core" : { "initially_open" : [ "solution_window_2" ] }})
+});
+
+
+var mwextUrlProtocolsVAL = {"tag":{"pre":"mw-tag-pre","nowiki":"mw-tag-nowiki","phptag":"text/x-php"},"func":[],"data":[]};
+var mwextTagsVAL = {"pre":true,"nowiki":true,"gallery":true,"indicator":true,"phptag":true,"source":true,"syntaxhighlight":true,"foxway":true}
+var mwextFunctionSynonymsVAL = [{"ns":"ns","nse":"nse","urlencode":"urlencode","lcfirst":"lcfirst","ucfirst":"ucfirst","lc":"lc","uc":"uc","localurl":"localurl","localurle":"localurle","fullurl":"fullurl","fullurle":"fullurle","canonicalurl":"canonicalurl","canonicalurle":"canonicalurle","formatnum":"formatnum","grammar":"grammar","gender":"gender","plural":"plural","#language":"language","padleft":"padleft","padright":"padright","anchorencode":"anchorencode","filepath":"filepath","pageid":"pageid","int":"int","#special":"special","#speciale":"speciale","#tag":"tag","#formatdate":"formatdate","#dateformat":"formatdate","#phptag":"phptag","#multimaps":"multimaps","#number_format":"number_format","#numbertext":"MAG_NUMBERTEXT","#moneytext":"MAG_MONEYTEXT","#if":"if","#ifeq":"ifeq","#switch":"switch","#ifexist":"ifexist","#ifexpr":"ifexpr","#iferror":"iferror","#expr":"expr","#time":"time","#timel":"timel","#rel2abs":"rel2abs","#titleparts":"titleparts","#len":"len","#pos":"pos","#rpos":"rpos","#sub":"sub","#count":"count","#replace":"replace","#explode":"explode","#urldecode":"urldecode","#foxway":"foxway","articlepath":"articlepath","server":"server","servername":"servername","scriptpath":"scriptpath","stylepath":"stylepath","currentuser":"MAG_CURRENTUSER","currentloggeduser":"MAG_CURRENTLOGGEDUSER","currentuserrealname":"MAG_CURRENTUSERREALNAME","logo":"MAG_LOGO","uuid":"MAG_UUID","userlanguagecode":"MAG_USERLANGUAGECODE"},{"NUMBEROFPAGES":"numberofpages","NUMBEROFUSERS":"numberofusers","NUMBEROFACTIVEUSERS":"numberofactiveusers","NUMBEROFARTICLES":"numberofarticles","NUMBEROFFILES":"numberoffiles","NUMBEROFADMINS":"numberofadmins","NUMBERINGROUP":"numberingroup","NUMINGROUP":"numberingroup","NUMBEROFEDITS":"numberofedits","DEFAULTSORT":"defaultsort","DEFAULTSORTKEY":"defaultsort","DEFAULTCATEGORYSORT":"defaultsort","PAGESINCATEGORY":"pagesincategory","PAGESINCAT":"pagesincategory","PAGESIZE":"pagesize","PROTECTIONLEVEL":"protectionlevel","NAMESPACEE":"namespacee","NAMESPACENUMBER":"namespacenumber","TALKSPACE":"talkspace","TALKSPACEE":"talkspacee","SUBJECTSPACE":"subjectspace","ARTICLESPACE":"subjectspace","SUBJECTSPACEE":"subjectspacee","ARTICLESPACEE":"subjectspacee","PAGENAME":"pagename","PAGENAMEE":"pagenamee","FULLPAGENAME":"fullpagename","FULLPAGENAMEE":"fullpagenamee","ROOTPAGENAME":"rootpagename","ROOTPAGENAMEE":"rootpagenamee","BASEPAGENAME":"basepagename","BASEPAGENAMEE":"basepagenamee","SUBPAGENAME":"subpagename","SUBPAGENAMEE":"subpagenamee","TALKPAGENAME":"talkpagename","TALKPAGENAMEE":"talkpagenamee","SUBJECTPAGENAME":"subjectpagename","ARTICLEPAGENAME":"subjectpagename","SUBJECTPAGENAMEE":"subjectpagenamee","ARTICLEPAGENAMEE":"subjectpagenamee","REVISIONID":"revisionid","REVISIONDAY":"revisionday","REVISIONDAY2":"revisionday2","REVISIONMONTH":"revisionmonth","REVISIONMONTH1":"revisionmonth1","REVISIONYEAR":"revisionyear","REVISIONTIMESTAMP":"revisiontimestamp","REVISIONUSER":"revisionuser","CASCADINGSOURCES":"cascadingsources","NAMESPACE":"namespace","DISPLAYTITLE":"displaytitle","!":"!","CURRENTMONTH":"currentmonth","CURRENTMONTH2":"currentmonth","CURRENTMONTH1":"currentmonth1","CURRENTMONTHNAME":"currentmonthname","CURRENTMONTHNAMEGEN":"currentmonthnamegen","CURRENTMONTHABBREV":"currentmonthabbrev","CURRENTDAY":"currentday","CURRENTDAY2":"currentday2","CURRENTDAYNAME":"currentdayname","CURRENTYEAR":"currentyear","CURRENTTIME":"currenttime","CURRENTHOUR":"currenthour","LOCALMONTH":"localmonth","LOCALMONTH2":"localmonth","LOCALMONTH1":"localmonth1","LOCALMONTHNAME":"localmonthname","LOCALMONTHNAMEGEN":"localmonthnamegen","LOCALMONTHABBREV":"localmonthabbrev","LOCALDAY":"localday","LOCALDAY2":"localday2","LOCALDAYNAME":"localdayname","LOCALYEAR":"localyear","LOCALTIME":"localtime","LOCALHOUR":"localhour","SITENAME":"sitename","CURRENTWEEK":"currentweek","CURRENTDOW":"currentdow","LOCALWEEK":"localweek","LOCALDOW":"localdow","REVISIONSIZE":"revisionsize","CURRENTVERSION":"currentversion","CURRENTTIMESTAMP":"currenttimestamp","LOCALTIMESTAMP":"localtimestamp","DIRECTIONMARK":"directionmark","DIRMARK":"directionmark","CONTENTLANGUAGE":"contentlanguage","CONTENTLANG":"contentlanguage"}];
+var CMoriginalJap = CodeMirror(document.getElementById("originalJap"), {
+  lineNumbers: true,
+  matchBrackets: true,
+  styleActiveLine: true,
+  lineWrapping: true,
+  mwextUrlProtocols: mwextUrlProtocolsVAL,
+  mwextTags: mwextTagsVAL,
+  mwextFunctionSynonyms: mwextFunctionSynonymsVAL,
+  mode:  "mediawiki",
+  value: source_Jap
+});
+var CMoriginalEng = CodeMirror(document.getElementById("originalEng"), {
+  lineNumbers: true,
+  matchBrackets: true,
+  styleActiveLine: true,
+  lineWrapping: true,
+  mwextUrlProtocols: mwextUrlProtocolsVAL,
+  mwextTags: mwextTagsVAL,
+  mwextFunctionSynonyms: mwextFunctionSynonymsVAL,
+  mode:  "mediawiki",
+  value: source_Eng
+});
+var CMoriginalPrev = CodeMirror(document.getElementById("originalPrev"), {
+  lineNumbers: true,
+  matchBrackets: true,
+  lineWrapping: true,
+  mwextUrlProtocols: mwextUrlProtocolsVAL,
+  mwextTags: mwextTagsVAL,
+  mwextFunctionSynonyms: mwextFunctionSynonymsVAL,
+  styleActiveLine: true,
+  mode:  "mediawiki",
+  value: source_editor3
+});
+var CMoriginalOrph = CodeMirror(document.getElementById("originalOrph"), {
+  lineNumbers: true,
+  matchBrackets: true,
+  lineWrapping: true,
+  styleActiveLine: true,
+  mwextUrlProtocols: mwextUrlProtocolsVAL,
+  mwextTags: mwextTagsVAL,
+  mwextFunctionSynonyms: mwextFunctionSynonymsVAL,
+  mode:  "mediawiki",
+  value: source_editor3
+});
+var CMvol1_window = CodeMirror(document.getElementById("volume0"), {
+  lineNumbers: true,
+  matchBrackets: true,
+  lineWrapping: true,
+  styleActiveLine: true,
+  mwextUrlProtocols: mwextUrlProtocolsVAL,
+  mwextTags: mwextTagsVAL,
+  mwextFunctionSynonyms: mwextFunctionSynonymsVAL,
+  mode:  "mediawiki",
+  value: source_Rus
+});
+var CMwindows = [CMoriginalJap, CMoriginalEng, CMoriginalPrev, CMoriginalOrph, CMvol1_window];
+var CMscroll,leftX,topY;
+
+
+CMwindows.forEach(function(item, i, arr) {
+  item.on('scroll', function(){
+    CMscroll = item.getScrollInfo();
+    CMwindows.forEach(function(item2, i2, arr2) {
+      if(item!=item2){
+        item2.scrollTo(CMscroll.left,CMscroll.top)
+      }
+    });
+  });
+});
+
+var Mass1JAP = [0,1,2,3,3,4,5,5];
+var Mass2ENG = [0,0,1,2,3,4,4,5];
+var Mass3RUS = [0,1,1,2,3,3,4,5];
+
+/*
+$('#originalEng .CodeMirror-code div:eq(0) pre span').html($('#originalEng .CodeMirror-code div:eq(0) pre span').html() + '\n')
+function addN(type, num, col){
+  var el = $('#original'+type+' .CodeMirror-code div:eq('+num+') pre span');
+  for (var i=0;i<col;i++){
+    el.html(el.html() + '\n')
+  }
+}
+*/
+
+function smartPasteListner(cm) {
+    return function(event) {
+        var data = event.clipboardData.getData('text/html');
+        if(data) {
+            //убераем лишние пробельные символы
+            data = data.replace(/([\n\r] *)+/g,'\n');
+            data = data.replace(/>\n</g,'><');
+            data = data.replace(/\s+/g,' ');
+            //экранируем теги руби избавляясь от стилей
+            data = data.replace(/<(\/?(rb|rp|rt|ruby))( +[^>]*?)?>/g,'[$1]');
+            //добавляем разрывы строк в места абзацев и брейков
+            data = data.replace(/(<p( [^>]*?)?>)?<\/?br( [^>]*?)?\/?>( <\/p>)?/g,'\n');
+            data = data.replace(/(<\/p>(\n*))?<p( [^>]*?)?>/g,'$2\n');
+            //удаляем все теги стиля и теги пространства имен вместе с содержимим
+            data = data.replace(/<([^: >]+:[^ >]+|style)( [^>]*?)?>.*?<\/\1>/g,'');
+            //удаляем текст после завершающего html
+            data = data.replace(/<\/html>.*/,'');
+            //обрабатываем вордовские сноски
+            var data_t = data;
+            do {
+                data = data_t;
+                data_t = data.replace(/<a[^>]+style ?= ?["']([^"']*;)?mso-(foot|end)note-id: ?([^"'; ]+) ?(;[^"']*)?["'].*?<\/a>((.|\r|\n)*?)<div[^>]+id ?= ?["']?\3["']?[^>]*>\s*((.|\r|\n)*?<\/div>)/g, '{{ref|$7}}$5');
+            } while(data != data_t);
+            data = data.replace(/<!\[if !support(Foot|End)notes\]>(.|\r|\n)*?<!\[endif\]>/g,'');
+            //обрабатываем жирность-курсив
+            data = data.replace(/<i( [^>]*?)?>(.*?)<\/i>/g,"''$2''");
+            data = data.replace(/<b( [^>]*?)?>(.*?)<\/b>/g,"'''$2'''");
+            //удаляем все теги
+            data = data.replace(/<\/?.+?>/g,'');
+            //чистка жирности-курсива
+            data = data.replace(/''''''/g,'');
+            data = data.replace(/''''/g,'');
+            //чистка примечаний
+            data = data.replace(/\{\{ref\| /g,'{{ref|');
+            //востановление руби
+            data = data.replace(/\[(\/?(rb|rp|rt|ruby))\]/g,'<$1>');
+            //удаляем разрывы с начала фрагмента
+            data = data.replace(/^\n/,'');
+            //заменяем ###
+            data = data.replace(/###/g,'{{Иллюстрация}}');
+            // заменяем неразрывный пробел
+            data = data.replace(/&nbsp;/g," ");
+            //заменяем %%%
+            //data = data.replace(/\n\n+/g,"\n");
+            data = data.replace(/%%%/g,'');
+            //чистка лишних пробелов и корректировка тире
+            data = data.replace(/  +/g,' ');
+            data = data.replace(/ ?\n ?/g,"\n");
+            data = data.replace(/\n[-–] ?/g,"\n— ");
+            data = data.replace(/ [-–] /g," — ");
+            data = data.replace(/([^ \n—])[—–]([^ \n—])/g,"$1-$1");
+            data = data.replace(/— —/g,"——");
+            data = data.replace(/— —/g,"——");
+            //console.log(data);
+            cm.getDoc().replaceSelection(data);
+            event.preventDefault();
+        }
+    }
+}
+CMwindows.forEach(function(item, i, arr) {
+  item.getWrapperElement().addEventListener("paste", smartPasteListner(item));
+});
+
+CMoriginalJap.on('change', function(cm, changeObj){
+  var cur=cm.getSearchCursor(/<ruby>.*?<\/ruby>/, changeObj.from)
+  while(cur.findNext()) {
+    if(cm.getDoc().findMarksAt(cur.from()).some(function(){return this.primary.className=='ruby';}))
+    break;
+    cm.getDoc().markText(cur.from(), cur.to(), {
+    className: 'ruby',
+    shared: true,
+    replacedWith: $(cur.pos.match[0]).get(0)
+    });
+  }
+})
---
+++ ITE2/ite.js
@@ -0,0 +1,256 @@
+
+
+
+
+    var myDocker = new wcDocker('.dockerContainer', {
+        allowDrawers: true,
+        responseRate: 10,
+        hideOnResize: true,
+        theme: "ruranobe",
+        allowContextMenu: false
+    });
+
+    var _showingInfo  = true;
+    var _savedLayout  = null;
+    var _chatterIndex = 1;
+
+    var defaultTitle = '<i class="icon-circle-arrow-right"></i> ';
+
+    myDocker.registerPanelType('Toolbox', {
+      faicon: 'icon-circle-arrow-right',
+      isPrivate: true,
+      onCreate: function(myPanel) {
+        // myPanel.layout().$table.css('padding', '5px');
+
+        // Constrain the sizing of this window so the user can't resize it.
+        myPanel.initSize(Infinity, 30);
+        myPanel.minSize(100, 30);
+        myPanel.maxSize(Infinity, 30);
+        myPanel.title(false);
+
+        // Do not allow the user to move or remove this panel, this will remove the title bar completely from the frame.
+        myPanel.moveable(false);
+        myPanel.closeable(false);
+        myPanel.scrollable(false, false);
+
+        var $button_bold = $('<button type="button" class="btn btn-default btn-xs" title="Жирный" data-insert="\'\'\'insert\'\'\'"><i class="fa fa-bold"></i></button>');
+        var $button_ital = $('<button type="button" class="btn btn-default btn-xs" title="Курсив"  data-insert="\'\'insert\'\'"><i class="fa fa-italic"></i></button>');
+        var $button_h2 = $('<button type="button" class="btn btn-default btn-xs" title="Заголовок 2-го уровня"  data-insert="==insert=="><i class="fa fa-header"></i>2</button>');
+        var $button_h3 = $('<button type="button" class="btn btn-default btn-xs" title="Заголовок 3-го уровня"  data-insert="===insert==="><i class="fa fa-header"></i>3</button>');
+        var $button_uh = $('<button type="button" class="btn btn-default btn-xs UnderHead" title="Подзаголовок"  data-insert="{{Подзаголовок|insert}}"><i class="fa fa-header"></i><i class="fa fa-align-center" style="font-size:80%"></i></button>');
+        var $button_pr = $('<button type="button" class="btn btn-default btn-xs" title="Примечание"  data-insert="{{ref|insert}}"><i class="fa fa-file-text"></i></button>');
+        var $button_com = $('<button type="button" class="btn btn-default btn-xs" title="Комментарий"  data-insert="<!— insert —>"><i class="fa fa-tag"></i></button>');
+        var $button_illus = $('<button type="button" class="btn btn-default btn-xs Illus" title="Иллюстрация" data-insert="{{Иллюстрация}}insert "><i class="fa fa-picture-o"></i></button>');
+        var $button_wiki = $('<button type="button" class="btn btn-default btn-xs" title="Ссылка на Википедию" data-insert="Подробнее на [insert Википедии]"><i class="fa fa-link"></i><span style="font-size: 72%; margin-left: -2px; vertical-align: super; font-weight: bold;">W</span></button>');
+
+        myPanel.layout().addItem($button_bold);
+        myPanel.layout().addItem($button_ital);
+        myPanel.layout().addItem($button_h2);
+        myPanel.layout().addItem($button_h3);
+        myPanel.layout().addItem($button_uh);
+        myPanel.layout().addItem($button_pr);
+        myPanel.layout().addItem($button_com);
+        myPanel.layout().addItem($button_illus);
+        myPanel.layout().addItem($button_wiki);
+      }
+    });
+    myDocker.registerPanelType('Statusbar', {
+      faicon: 'icon-circle-arrow-right',
+      isPrivate: true,
+      onCreate: function(myPanel) {
+        // myPanel.layout().$table.css('padding', '5px');
+
+        // Constrain the sizing of this window so the user can't resize it.
+        myPanel.initSize(Infinity, 30);
+        myPanel.minSize(100, 30);
+        myPanel.maxSize(Infinity, 30);
+        myPanel.title(false);
+
+        // Do not allow the user to move or remove this panel, this will remove the title bar completely from the frame.
+        myPanel.moveable(false);
+        myPanel.closeable(false);
+        myPanel.scrollable(false, false);
+
+        var $qwe = $('<span>SOME TEXT</span>');
+        myPanel.layout().addItem($qwe);
+      }
+    });
+    myDocker.registerPanelType('VolumeTree', {
+      faicon: 'icon-circle-arrow-right',
+      isPrivate: true,
+      onCreate: function(myPanel) {
+        // myPanel.layout().$table.css('padding', '5px');
+        var $treediv = $('<div id="volumestree"></div>')
+        var $tree = $('<ul></ul');
+        for (var i=1; i<6;i++){
+        var $volume1 = $('<li id="solution_window_'+i+'">'
+                            + '<a href="#">Том '+i+'</a>'
+                                + '<ul>'
+                                    + '<li id="v'+i+'_p1"><a href="#">Пролог</a></li>'
+                                    + '<li id="v'+i+'_p2"><a href="#">Глава 1</a></li>'
+                                    + '<li id="v'+i+'_p3"><a href="#">Глава 2</a></li>'
+                                + '</ul>'
+                            + '</ul>'
+                        + '</li>');
+        $tree.append($volume1);
+
+        }
+        $treediv.append($tree);
+
+        myPanel.layout().addItem($treediv);
+      }
+    });
+    myDocker.registerPanelType('TranG', {
+      faicon: 'icon-circle-arrow-right',
+      isPrivate: true,
+      onCreate: function(myPanel) {
+        var $qwe = $('<div id="TranG"></div>');
+        myPanel.layout().addItem($qwe);
+      }
+    });
+    myDocker.registerPanelType('TranM', {
+      faicon: 'icon-circle-arrow-right',
+      isPrivate: true,
+      onCreate: function(myPanel) {
+        var $qwe = $('<div id="TranM"></div>');
+        myPanel.layout().addItem($qwe);
+      }
+    });
+    myDocker.registerPanelType('TranSin', {
+      faicon: 'icon-circle-arrow-right',
+      isPrivate: true,
+      onCreate: function(myPanel) {
+        var $qwe = $('<div id="TranSin"></div>');
+        myPanel.layout().addItem($qwe);
+      }
+    });
+
+
+    myDocker.registerPanelType('volume', {
+        faicon: 'icon-circle-arrow-right',
+        isPrivate: true,
+        onCreate: function(myPanel, options) {
+            var volL = $('.volume').length
+            var $qwe = $('<div class="volume" id="volume'+volL+'"></div>');
+            myPanel.title('Глава ' + (volL+1));
+            myPanel.layout().addItem($qwe);
+        }
+    });
+    myDocker.registerPanelType('GJap', {
+        faicon: 'icon-circle-arrow-right',
+        isPrivate: true,
+        onCreate: function(myPanel, options) {
+            var $qwe = $('<div id="GJap"></div>');
+            myPanel.layout().addItem($qwe);
+        }
+    });
+    myDocker.registerPanelType('GEn', {
+        faicon: 'icon-circle-arrow-right',
+        isPrivate: true,
+        onCreate: function(myPanel, options) {
+            var $qwe = $('<div id="GEn"></div>');
+            myPanel.layout().addItem($qwe);
+        }
+    });
+    myDocker.registerPanelType('Jap', {
+        faicon: 'icon-circle-arrow-right',
+        isPrivate: true,
+        onCreate: function(myPanel, options) {
+            var $qwe = $('<div id="Jap"></div>');
+            myPanel.layout().addItem($qwe);
+        }
+    });
+    myDocker.registerPanelType('Fresh', {
+        faicon: 'icon-circle-arrow-right',
+        isPrivate: true,
+        onCreate: function(myPanel, options) {
+            var $qwe = $('<div id="Fresh"></div>');
+            myPanel.layout().addItem($qwe);
+        }
+    });
+
+
+    myDocker.registerPanelType('originalJap', {
+        faicon: 'icon-circle-arrow-right',
+        isPrivate: true,
+        onCreate: function(myPanel, options) {
+            var $qwe = $('<div id="originalJap"></div>');
+            myPanel.layout().addItem($qwe);
+            myPanel.scrollable(false, false);
+        }
+    });
+    myDocker.registerPanelType('originalEng', {
+        faicon: 'icon-circle-arrow-right',
+        isPrivate: true,
+        onCreate: function(myPanel, options) {
+            var $qwe = $('<div id="originalEng"></div>');
+            myPanel.layout().addItem($qwe);
+            myPanel.scrollable(false, false);
+        }
+    });
+    myDocker.registerPanelType('originalPrev', {
+        faicon: 'icon-circle-arrow-right',
+        isPrivate: true,
+        onCreate: function(myPanel, options) {
+            var $qwe = $('<div id="originalPrev"></div>');
+            myPanel.layout().addItem($qwe);
+            myPanel.scrollable(false, false);
+        }
+    });
+    myDocker.registerPanelType('originalOrph', {
+        faicon: 'icon-circle-arrow-right',
+        isPrivate: true,
+        onCreate: function(myPanel, options) {
+            var $qwe = $('<div id="originalOrph"></div>');
+            myPanel.layout().addItem($qwe);
+            myPanel.scrollable(false, false);
+        }
+    });
+
+/* INITIALIZATION */
+    myDocker.startLoading('Loading...');
+    var volumePanel = myDocker.addPanel('volume', wcDocker.DOCK.RIGHT,null, {w: "33%"});
+    var volumePanel1 = myDocker.addPanel('volume', wcDocker.DOCK.STACKED, volumePanel);
+    var volumePanel2 = myDocker.addPanel('volume', wcDocker.DOCK.STACKED, volumePanel);
+
+    var treePanel = myDocker.addPanel('VolumeTree', wcDocker.DOCK.LEFT, null, {w: "24%"});
+    var translateG = myDocker.addPanel('TranG', wcDocker.DOCK.BOTTOM, treePanel);
+    var translateM = myDocker.addPanel('TranM', wcDocker.DOCK.STACKED, translateG);
+    var translateSin = myDocker.addPanel('TranSin', wcDocker.DOCK.STACKED, translateG);
+    var toolBoxPanel = myDocker.addPanel('Toolbox', wcDocker.DOCK.TOP);
+    var statusbarPanel = myDocker.addPanel('Statusbar', wcDocker.DOCK.BOTTOM);
+
+
+    var GJap = myDocker.addPanel('GJap', wcDocker.DOCK.BOTTOM, volumePanel, {h: 200});
+    var GEn = myDocker.addPanel('GEn', wcDocker.DOCK.STACKED, GJap);
+    var Jap = myDocker.addPanel('Jap', wcDocker.DOCK.STACKED, GJap);
+    var Fresh = myDocker.addPanel('Fresh', wcDocker.DOCK.STACKED, GJap);
+
+    var originalJap = myDocker.addPanel('originalJap', wcDocker.DOCK.LEFT, volumePanel, {w: "33%"});
+    var originalEng = myDocker.addPanel('originalEng', wcDocker.DOCK.STACKED, originalJap);
+    var originalPrev = myDocker.addPanel('originalPrev', wcDocker.DOCK.STACKED, originalJap);
+    var originalOrph = myDocker.addPanel('originalOrph', wcDocker.DOCK.STACKED, originalJap);
+
+
+// SETTING
+    treePanel.title("Дерево Томов");
+    translateG.title("Google");
+    translateM.title("Multitran");
+    translateSin.title("Синоним");
+    GJap.title("Google Japanese");
+    GEn.title("Google English");
+    Jap.title("Japanese");
+    Fresh.title("Свежий Взгляд");
+    originalJap.title("Japanese");
+    originalEng.title("English");
+    originalPrev.title("Предыдущий");
+    originalOrph.title("Орфус");
+    $('#originalJap').height(originalJap.height()-10).click();
+    $('#originalEng').height(originalEng.height()-10).click();
+    $('#originalPrev').height(originalPrev.height()-10).click();
+    $('#originalOrph').height(originalOrph.height()-10).click();
+
+/* INITIALIZATION */
+    myDocker.on(wcDocker.EVENT.LOADED, function() {
+      myDocker.finishLoading(500);
+    });
