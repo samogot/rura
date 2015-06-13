584df266a6383ea15a0bfa1f10a7a7a00936f474
Antosik <grig_anton96@mail.ru>
2015-06-13 15:37:49 +0400

    Скрипт Админки
    Фикс бага с подсериями

--- js/adminka.js
+++ js/adminka.js
@@ -2,12 +2,12 @@ $('button[type=submit]').on('click',
     function(){
         var parent = $(this).closest(".FormAjax");
         if( parent.hasClass("FormUpdate") ){
-            AdminOnUpdate(parent.children("#volume_id").val(), parent.data('type'));
+            AdminOnUpdate(parent, parent.children("#volume_id").val(), parent.data('type'));
         }
         return false;
     }
 )
-$('button.btn-success').on('click',
+$('body').on('click','button.btn-success',
     function(){
         var type = $(this).data('type');
         var length = $('[data-parent="#'+type+'"]').length
@@ -15,45 +15,40 @@ $('button.btn-success').on('click',
         return false;
     }
 )
-$('button.btn-danger').on('click',
-    function(){
-        var type = $(this).data('type');
-        var Needdelete = $('[data-parent="#'+type+'"].info')
-        AdminOnDelete(Needdelete.data('type'));
-        return false;
-    }
-)
 $('body').on('change','input, textarea',
     function(){
         var parent = $(this).closest(".FormAjax");
         if( parent.hasClass("FormUpdate") ){
-            AdminOnUpdate(parent.children("#volume_id").val(), parent.data('type'));
+            AdminOnUpdate(parent, parent.children("#volume_id").val(), parent.data('type'));
         }
         return false;
     }
 )
 function AdminOnDelete (type) {
     var projectId = $('[data-type="'+type+'"').children('#volume_id').val();
+    console.log(type);
     alert('Need Delete: '+type);
 }
-function AdminOnUpdate (projectId, type) {
+function AdminOnUpdate (parent, projectId, type) {
     var projectInfo = {};
-    for (var i = 0 ; i < $('.FormUpdate input[type=text]').length ; i++){
-        var el = $('.FormUpdate input[type=text]:eq('+i+')');
+    console.log(parent);
+    for (var i = 0 ; i < parent.find('input[type=text]').length ; i++){
+        var el = parent.find('input[type=text]:eq('+i+')');
         projectInfo[el.attr('name')] = el.val();
     }
-    for (var i = 0 ; i < $('.FormUpdate input[type=checkbox]').length ; i++){
-        var el = $('.FormUpdate input[type=checkbox]:eq('+i+')');
+    for (var i = 0 ; i < parent.find('input[type=checkbox]').length ; i++){
+        var el = parent.find('input[type=checkbox]:eq('+i+')');
         projectInfo[el.attr('name')] = el.prop("checked");;
     }
-    for (var i = 0 ; i < $('.FormUpdate textarea').length ; i++){
-        var el = $('.FormUpdate textarea:eq('+i+')');
+    for (var i = 0 ; i < parent.find('textarea').length ; i++){
+        var el = parent.find('textarea:eq('+i+')');
         projectInfo[el.attr('name')] = el.val();
     }
     var info = {'type': type, 'projectId': projectId, 'projectInfo': projectInfo}
+    console.log(info);
     alert(JSON.stringify(info));
 }
 function AdminOnInsert (type) {
     var projectId = $('[data-type="'+type+'"').children('#volume_id').val();
-    AdminOnUpdate (projectId, type);
+    AdminOnUpdate ($('[data-type="'+type+'"'), projectId, type);
 }
