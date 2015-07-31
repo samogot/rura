65fe0d29254aad0510cda090ab656d77e7e88966
Antosik <grig_anton96@mail.ru>
2015-07-31 19:00:21 +0400

    Страница с текстом - Галерея
    Ресайз колесиком + перетягивание - https://trello.com/c/hnOtUCD5
    Кнопки листания - https://trello.com/c/PW2M1000

--- js/page.text.js
+++ js/page.text.js
@@ -1,5 +1,29 @@
  $(document).ready(function() {
-        $(".fancybox").fancybox();
+        $(".fancybox")
+            .attr('rel', 'gallery')
+            .fancybox({
+                mouseWheel: false,
+                padding: 0,
+                margin: 5,
+                scrolling: 'no',
+                autoResize: false,
+                fitToView: true,
+                afterLoad: function () {
+                    var fancy=this;
+                    this.outer.mousewheel(function (e,a1) {
+                        fancy.wrap.css('width','auto');
+                        var d=Math.pow(1.1,a1);
+                        var x=e.pageX-fancy.inner.offset().left;
+                        var y=e.pageY-fancy.inner.offset().top;
+                        fancy.inner.width(fancy.inner.width()*d);
+                        fancy.inner.height(fancy.inner.height()*d);
+                        fancy.wrap.css('left',fancy.wrap.position().left+x*(1-d));
+                        fancy.wrap.css('top',fancy.wrap.position().top+y*(1-d));
+                        return false;
+                    });
+                    this.wrap.draggable();
+                }
+            });
  });
 
 /* AFFIX */
